04c3e03b64bee444d80372c3d5a4bd069cd9ab72
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-01-21 23:23:26 +0200

    Created Shorten btn

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,12 @@ function App() {
         <p className='text-[18px] text-gray-500 px-5 mb-8'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
         <div>Get Started</div>
       </div>
-      <form className='flex flex-col justify-center items-center w-[327px] h-[182px] bg-primary-purple rounded-xl mt-8'>
+      <form className='flex flex-col justify-center items-center w-[327px] h-[160px] bg-primary-purple rounded-xl mt-8'>
         <div className='flex flex-col'>
             <label htmlFor="name" className='mb-2'></label>
-              <input type="text" id="name" placeholder='Shorten a link here...'
-              className='text-[16px] text-gray-700 w-[279px] h-[50px] rounded-md bg-white cursor-pointer pl-4'/>
+            <input type="text" id="name" placeholder='Shorten a link here...'
+            className='text-[16px] text-gray-700 w-[279px] h-[50px] rounded-md bg-white cursor-pointer pl-4'/>
+            <button className='w-[279px] h-[50px] rounded-md bg-primary-blue text-[18px] text-white font-bold mt-4'>Shorten it!</button>
         </div>
       </form>
       </div>
