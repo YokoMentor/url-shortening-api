409c0d1a6965c8b25da80c3ca425728c5ce53ba1
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-01-22 22:27:35 +0200

    Positioned illustration

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import illustration from './images/illustration-working.svg'
 function App() {
   return (
     <div className="flex flex-col min-h-screen justify-center font-display text-center">
-      <div className='flex flex-row justify-between px-5 mb-9'>
+      <div className='flex flex-row justify-between px-5 mb-9 mt-10'>
         <img src={logo} alt="logo"></img>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="size-9 stroke-gray-500">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </div>
       <div className='flex flex-col items-center'>
+      <div className='flex flex-col'>
+        <img src={illustration} alt="illustration" className='w-[502px] h-[330px] object-cover mb-9 object-left translate-x-6'></img>
         <div className='flex flex-col'>
-        <img src={illustration} alt="illustration" className='w-[502px] h-[330px] object-cover object-center object-[-18%] mb-9'></img>
-        <h1 className='font-bold text-[40px] leading-[46px] text-gray-950 mb-4'>More than just shorter links</h1>
-        <p className='text-[18px] text-gray-500 px-5 mb-8'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
-        <div>Get Started</div>
+          <h1 className='font-bold text-[40px] leading-[46px] text-gray-950 mb-4'>More than just shorter links</h1>
+          <p className='text-[18px] text-gray-500 px-5 mb-8'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+          <div>Get Started</div>
+        </div>
       </div>
       <div className='relative flex flex-col justify-center items-center mt-30'>
         <form className='flex flex-col justify-center items-center w-[327px] h-[160px] bg-primary-purple rounded-xl absolute bottom'>
