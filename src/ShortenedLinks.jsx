c79c9f8bb9445c69abcd5ad0258e620ea9a1a053
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-02-10 00:28:48 +0200

    Fixed divider

--- src/ShortenedLinks.jsx
+++ src/ShortenedLinks.jsx
@@ -5,9 +5,10 @@ function ShortenedLinks ({link}) {
   return (
     <div className='flex flex-col justify-center items-center w-full bg-bg-gray mb-21'>
       <div  className='flex flex-col justify-center items-center w-[327px] md:w-[1110px] bg-white rounded-xl'>
-        <div className='text-gray-950'>{link}</div>
-        <div className='text-primary-blue'>Shortened Link</div>
-        <button className='w-[279px] h-[48px] md:w-[189px] md:h-[65px] rounded-md bg-primary-blue text-[18px] md:text-[20px] text-white font-bold mt-4 md:mt-[52px] md:ml-6 cursor-pointer mb-[24px]'>Copy</button>
+        <div className='text-gray-950 mt-[16px] mb-3'>{link}</div>
+        <hr className='border-bg-gray border-1 w-full mb-3'/>
+        <div className='text-primary-blue mb-2'>Shortened Link</div>
+        <button className='w-[279px] h-[40px] md:w-[189px] md:h-[65px] rounded-md bg-primary-blue text-[18px] md:text-[20px] text-white font-bold md:mt-[52px] md:ml-6 cursor-pointer mb-[16px]'>Copy</button>
       </div>
     </div>
   )
