24e2321328b6a168eb95f885f88a36a210dd29d7
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-02-11 23:09:40 +0200

    Adjusted shortening box for landscape

--- src/ShortenedLinks.jsx
+++ src/ShortenedLinks.jsx
@@ -4,13 +4,13 @@ import './App.css'
 function ShortenedLinks ({link}) {
   return (
     <div className='flex flex-col justify-center items-center w-full bg-bg-gray mb-21'>
-      <div  className='flex flex-col justify-center items-center w-[327px] md:w-[1110px] bg-white rounded-xl'>
-        <div className='flex flex-col w-full px-6 text-left'>
-          <div className='text-gray-950 mt-[16px] mb-3'>{link}</div>
-          <hr className='border-bg-gray border-1 w-full mb-3'/>
-          <div className='text-primary-blue mb-2'>Shortened Link</div>
+      <div  className='flex flex-col md:flex-row justify-center items-center w-[327px] md:w-[1110px] md:h-[72px] bg-white rounded-xl'>
+        <div className='flex flex-col md:flex-row w-full px-6 text-left md:justify-between md:items-center md:text-[18px]'>
+          <div className='text-gray-950 mt-[16px] md:mt-0 mb-3 md:mb-0'>{link}</div>
+          <hr className='border-bg-gray border-1 md:border-0 w-full md:w-0 mb-3 md:mb-0'/>
+          <div className='text-primary-blue mb-2 md:mb-0'>Shortened Link</div>
         </div>
-        <button className='w-[279px] h-[40px] md:w-[189px] md:h-[65px] rounded-md bg-primary-blue text-[18px] md:text-[20px] text-white font-bold md:mt-[52px] md:ml-6 cursor-pointer mb-[16px]'>Copy</button>
+        <button className='w-[279px] h-[40px] md:w-[120px] md:h-[40px] rounded-md bg-primary-blue text-[18px] md:text-[16px] text-white font-bold cursor-pointer mb-[16px] md:mb-0 md:mr-6'>Copy</button>
       </div>
     </div>
   )
