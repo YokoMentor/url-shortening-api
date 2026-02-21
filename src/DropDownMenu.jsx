081e1e20c94c345ddadeeeeefda4e885724e5969
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-02-21 22:41:09 +0200

    DropDownMenu layout

--- src/DropDownMenu.jsx
+++ src/DropDownMenu.jsx
@@ -5,13 +5,13 @@ function DropDownMenu() {
 
   return (
   <div className='flex justify-center items-center relative'>
-    <div className='flex flex-col justify-center items-center w-[328px] h-[383px] text-[20px] text-white font-bold bg-primary-purple rounded-md ml-[24px] mr-[24px] absolute top-0'>
-      <div>Features</div>
-      <div>Pricing</div>
-      <div>Resources</div>
-      <hr />
-      <div>Login</div>
-      <button className='w-[198px] h-[56px] rounded-full bg-primary-blue hover:bg-hover mb-28 cursor-pointer'>Sign up</button>
+    <div className='flex flex-col justify-center items-center w-[328px] text-[18px] text-white font-bold bg-primary-purple rounded-md ml-[24px] mr-[24px] absolute top-0 pl-6 pr-6'>
+      <div className='mb-8 mt-10'>Features</div>
+      <div className='mb-7'>Pricing</div>
+      <div className='mb-7'>Resources</div>
+      <hr className='border-bg-gray border-1 md:border-0 w-full mb-8'/>
+      <div className='mb-6'>Login</div>
+      <button className='w-[279px] h-[48px] rounded-full bg-primary-blue hover:bg-hover mb-11 cursor-pointer'>Sign up</button>
     </div>
   </div>
   )
